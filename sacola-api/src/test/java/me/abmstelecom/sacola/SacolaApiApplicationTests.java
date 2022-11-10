d7ebae7dd6b2c7569da350f0b7b50dd2494cd2c0
arduinobymyself <arduinobymyself@gmail.com>
2022-11-10 13:43:48 -0300

    subindo arquivos

---
+++ sacola-api/src/test/java/me/abmstelecom/sacola/SacolaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.abmstelecom.sacola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacolaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
